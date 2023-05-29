1bb5995add9be6fa1fdd539ab51cebdffa7e7f1e
Helios59 <h.reyes.r@edu.utc.mx>
2023-05-29 13:11:15 -0600

    validacion en js

--- 04Formulario/js/validacion.js
+++ 04Formulario/js/validacion.js
@@ -1,86 +1,71 @@
-// JS es multiparadigmico y vamos a iniciar a la programacion por funciones.
+// JS es multiparadigmático y vamos a iniciar con programación por funciones.
 
-function validar(formulario){
-    // Hay varias formas para poder obtener los parametros del formulario.
-    if(formulario.nombre.value.length <= 3){
-        alert("Escriba mas de 3 caracteres en el campo nombre");
-        formulario.nombre.focus();
-    }
-
-    var checkOk = "qwertyuiopasdfghjklñzxcvbnm"+
-    "QWERTYUIOPASDFGHJKLÑZXCVBNM ";
-
-    var checkString = formulario.nombre.value;
-
-    var todoesvalido = true;
-    /*
-    JavaScript es un lenguajes de programacion no typado
-    eso quiere decier que solo maneja 3 tipos
-    de variable de dato:
-
-    var es una variable cambiante con acceso publico.
-
-    let es una variable de acceso restringido.
-
-    const es una constante de acceso unico.
-    */
-
-    for(var i = 0; i < checkString.length; i++) {
-        var ch = checkString.charAt(i);
-        for(var j = 0; j < checkOk.length; i++) {
-            if(ch == checkOk.charAt(J)) {
-                break;
-            }
-        }
-        if(j == checkOk.length) {
-            todoesvalido = false;
-            break;
-        }
-    }
-    if(!todoesvalido) {
-        alert("Escriba unicamente letras en el campo nombre");
+function validar(formulario) {
+    // Hay varias formas de obtener los parámetros del formulario.
+    if (formulario.nombre.value.length <= 3) {
+        alert("Escriba más de 3 caracteres en el campo nombre");
         formulario.nombre.focus();
         return false;
     }
 
-    var checkOk = "1234567890";
+    var checkOk = "qwertyuiopasdfghjklñzxcvbnm" +
+        "QWERTYUIOPASDFGHJKLÑZXCVBNM ";
 
     var checkString = formulario.nombre.value;
 
-    var todoesvalido = true;
-    /*
-    JavaScript es un lenguajes de programacion no typado
-    eso quiere decier que solo maneja 3 tipos
-    de variable de dato:
+    var todoEsValido = true;
 
-    var es una variable cambiante con acceso publico.
-
-    let es una variable de acceso restringido.
-
-    const es una constante de acceso unico.
-    */
-
-    for(var i = 0; i < checkString.length; i++) {
+    for (var i = 0; i < checkString.length; i++) {
         var ch = checkString.charAt(i);
-        for(var j = 0; j < checkOk.length; i++) {
-            if(ch == checkOk.charAt(J)) {
+        var isLetter = false;
+        for (var j = 0; j < checkOk.length; j++) {
+            if (ch === checkOk.charAt(j)) {
+                isLetter = true;
                 break;
             }
         }
-        if(j == checkOk.length) {
-            todoesvalido = false;
+        if (!isLetter) {
+            todoEsValido = false;
             break;
         }
     }
-    if(!todoesvalido) {
-        alert("Escriba unicamente numeros en el campo edad");
+
+    if (!todoEsValido) {
+        alert("Escriba únicamente letras en el campo nombre");
+        formulario.nombre.focus();
+        return false;
+    }
+
+    checkOk = "1234567890";
+    checkString = formulario.edad.value;
+    todoEsValido = true;
+
+    for (var i = 0; i < checkString.length; i++) {
+        var ch = checkString.charAt(i);
+        var isNumber = false;
+        for (var j = 0; j < checkOk.length; j++) {
+            if (ch === checkOk.charAt(j)) {
+                isNumber = true;
+                break;
+            }
+        }
+        if (!isNumber) {
+            todoEsValido = false;
+            break;
+        }
+    }
+
+    if (!todoEsValido) {
+        alert("Escriba únicamente números en el campo edad");
         formulario.edad.focus();
         return false;
     }
 
     var txt = formulario.email.value;
+    var emailRegex = /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/;
 
-    var b = /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/;
-    
+    alert("El email es " + (emailRegex.test(txt) ? "" : "no ") + "válido");
+
+    return emailRegex.test(txt);
 }
 
