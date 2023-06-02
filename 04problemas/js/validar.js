38488501c9610df054222a6cf927e9ce0a9d2b74
Helios59 <h.reyes.r@edu.utc.mx>
2023-06-01 20:22:57 -0600

    arreglo js y html de la tarea

--- 04problemas/js/validar.js
+++ 04problemas/js/validar.js
@@ -10,22 +10,17 @@ function calcularGanancia() {
 }
 
 function calcularComisiones() {
-  const ventas = parseFloat(document.getElementById("ventas").value);
-  if (isNaN(ventas) || ventas < 0 || ventas > 300000) {
-    alert("Por favor, ingresa un número válido y no negativo en el campo de ventas. El máximo permitido es 300,000.");
-    return;
-  }
+  var venta1 = parseFloat(document.getElementById("venta1").value);
+  var venta2 = parseFloat(document.getElementById("venta2").value);
+  var venta3 = parseFloat(document.getElementById("venta3").value);
+  var sueldoBase = parseFloat(document.getElementById("sueldo-base").value);
 
-  const sueldoBase = parseFloat(document.getElementById("sueldo-base").value);
-  if (isNaN(sueldoBase) || sueldoBase < 0 || sueldoBase > 300000) {
-    alert("Por favor, ingresa un número válido y no negativo en el campo de sueldo base. El máximo permitido es 300,000.");
-    return;
-  }
+  var comisiones = (venta1 + venta2 + venta3) * 0.1;
+  var total = sueldoBase + comisiones;
 
-  const comisiones = ventas * 0.1;
-  const total = sueldoBase + comisiones;
-  document.getElementById("comisiones").textContent = `Comisiones: $${comisiones.toFixed(2)}`;
-  document.getElementById("total").textContent = `Total a recibir: $${total.toFixed(2)}`;
+  document.getElementById("comisiones").innerHTML = "Las comisiones por las tres ventas serán de: " + comisiones.toFixed(2);
+  document.getElementById("total").innerHTML = "El total que recibirá en el mes será de: " + total.toFixed(2);
+  
 }
 
 function calcularDescuento() {
@@ -42,32 +37,32 @@ function calcularDescuento() {
 
 function calcularCalificacionFinal() {
   const califParcial1 = parseFloat(document.getElementById("calif-parcial1").value);
-  if (isNaN(califParcial1) || califParcial1 < 0 || califParcial1 > 300000) {
-    alert("Por favor, ingresa un número válido y no negativo en el campo de calificación parcial 1. El máximo permitido es 300,000.");
+  if (isNaN(califParcial1) || califParcial1 < 0 || califParcial1 > 100.99) {
+    alert("Por favor, ingresa un número válido y no negativo en el campo de calificación parcial 1. El máximo permitido es 100.99.");
     return;
   }
 
   const califParcial2 = parseFloat(document.getElementById("calif-parcial2").value);
-  if (isNaN(califParcial2) || califParcial2 < 0 || califParcial2 > 300000) {
-    alert("Por favor, ingresa un número válido y no negativo en el campo de calificación parcial 2. El máximo permitido es 300,000.");
+  if (isNaN(califParcial2) || califParcial2 < 0 || califParcial2 > 100.99) {
+    alert("Por favor, ingresa un número válido y no negativo en el campo de calificación parcial 2. El máximo permitido es 100.99.");
     return;
   }
 
   const califParcial3 = parseFloat(document.getElementById("calif-parcial3").value);
-  if (isNaN(califParcial3) || califParcial3 < 0 || califParcial3 > 300000) {
-    alert("Por favor, ingresa un número válido y no negativo en el campo de calificación parcial 3. El máximo permitido es 300,000.");
+  if (isNaN(califParcial3) || califParcial3 < 0 || califParcial3 > 100.99) {
+    alert("Por favor, ingresa un número válido y no negativo en el campo de calificación parcial 3. El máximo permitido es 100.99.");
     return;
   }
 
   const califExamen = parseFloat(document.getElementById("calif-examen").value);
-  if (isNaN(califExamen) || califExamen < 0 || califExamen > 300000) {
-    alert("Por favor, ingresa un número válido y no negativo en el campo de calificación del examen. El máximo permitido es 300,000.");
+  if (isNaN(califExamen) || califExamen < 0 || califExamen > 100.99) {
+    alert("Por favor, ingresa un número válido y no negativo en el campo de calificación del examen. El máximo permitido es 100.99.");
     return;
   }
 
   const califTrabajo = parseFloat(document.getElementById("calif-trabajo").value);
-  if (isNaN(califTrabajo) || califTrabajo < 0 || califTrabajo > 300000) {
-    alert("Por favor, ingresa un número válido y no negativo en el campo de calificación del trabajo. El máximo permitido es 300,000.");
+  if (isNaN(califTrabajo) || califTrabajo < 0 || califTrabajo > 100.99) {
+    alert("Por favor, ingresa un número válido y no negativo en el campo de calificación del trabajo. El máximo permitido es 100.99.");
     return;
   }
 
@@ -79,14 +74,14 @@ function calcularCalificacionFinal() {
 
 function calcularPorcentaje() {
   const totalEstudiantes = parseFloat(document.getElementById("total-estudiantes").value);
-  if (isNaN(totalEstudiantes) || totalEstudiantes < 0 || totalEstudiantes > 300000) {
-    alert("Por favor, ingresa un número válido y no negativo en el campo de total de estudiantes. El máximo permitido es 300,000.");
+  if (isNaN(totalEstudiantes) || totalEstudiantes < 0 || totalEstudiantes > 200.99) {
+    alert("Por favor, ingresa un número válido y no negativo en el campo de total de estudiantes. El máximo permitido es 200 estudiantes.");
     return;
   }
 
   const totalHombres = parseFloat(document.getElementById("total-hombres").value);
-  if (isNaN(totalHombres) || totalHombres < 0 || totalHombres > 300000) {
-    alert("Por favor, ingresa un número válido y no negativo en el campo de total de hombres. El máximo permitido es 300,000.");
+  if (isNaN(totalHombres) || totalHombres < 0 || totalHombres > totalEstudiantes) {
+    alert("Por favor, ingresa un número válido y no negativo en el campo de total de hombres. El máximo permitido es el valor ingresado en el campo de total de estudiantes.");
     return;
   }
 
@@ -97,19 +92,41 @@ function calcularPorcentaje() {
   document.getElementById("porcentaje-mujeres").textContent = `Porcentaje de Mujeres: ${porcentajeMujeres.toFixed(2)}%`;
 }
 
+
 function calcularEdad() {
+  const diaNacimiento = parseInt(document.getElementById("dia-nacimiento").value);
+  const mesNacimiento = parseInt(document.getElementById("mes-nacimiento").value);
   const anioNacimiento = parseInt(document.getElementById("anio-nacimiento").value);
-  if (isNaN(anioNacimiento) || anioNacimiento < 0 || anioNacimiento > 300000) {
-    alert("Por favor, ingresa un número válido y no negativo en el campo de año de nacimiento. El máximo permitido es 300,000.");
+
+  if (isNaN(diaNacimiento) || diaNacimiento < 1 || diaNacimiento > 31) {
+    alert("Por favor, ingresa un día válido (entre 1 y 31) no puede ser negativo y no puede tener punto.");
+    return;
+  }
+
+  if (isNaN(mesNacimiento) || mesNacimiento < 1 || mesNacimiento > 12) {
+    alert("Por favor, ingresa un mes válido (entre 1 y 12) no puede ser negativo y no puede tener punto.");
+    return;
+  }
+
+  if (isNaN(anioNacimiento) || anioNacimiento < 1900 || anioNacimiento > 2023) {
+    alert("Por favor, ingresa un año válido (entre 1900 y 2023) no puede ser negativo y no puede tener punto.");
     return;
   }
 
   const fechaActual = new Date();
   const anioActual = fechaActual.getFullYear();
-  const edad = anioActual - anioNacimiento;
+  const mesActual = fechaActual.getMonth() + 1;
+  const diaActual = fechaActual.getDate();
+
+  let edad = anioActual - anioNacimiento;
+
+  if (mesActual < mesNacimiento || (mesActual === mesNacimiento && diaActual < diaNacimiento)) {
+    edad--;
+  }
 
   document.getElementById("edad").textContent = `Edad: ${edad} años`;
 }
+
 
 function borrarFormulario() {
   document.getElementById("problem-form").reset();
